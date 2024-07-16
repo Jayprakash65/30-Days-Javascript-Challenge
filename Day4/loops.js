f87c38c1fd694b0bfc6a6fc2961ed449e4938a9e
Jayprakash Yadab <jayprakashyadab65@gmail.com>
2024-07-16 16:39:35 +0530

    loops

--- Day4/loops.js
+++ Day4/loops.js
@@ -1,90 +1,90 @@
-// // Day 4: Loops
-// // Tasks/Activities:
-// // Activity 1: For Loop
+// Day 4: Loops
+// Tasks/Activities:
+// Activity 1: For Loop
 
-// // . Task 1: Write a program to print numbers from 1 to 10 using a for loop.
+// . Task 1: Write a program to print numbers from 1 to 10 using a for loop.
 
-// for (let i = 1; i <= 10; i++){
-//     console.log(i);
-// }
-
-
-
-// // . Task 2: Write a program to print the multiplication table of 5 using a for loop.
-
-// let num = 5
-// for (let i = 1; i <= 10; i++) {
-//     console.log(num * i);
-// }
+for (let i = 1; i <= 10; i++){
+    console.log(i);
+}
 
 
 
+// . Task 2: Write a program to print the multiplication table of 5 using a for loop.
 
-
-// // Activity 2: While Loop
-
-// // . Task 3: Write a program to calculate the sum of numbers from 1 to 10 using a while loop.
-
-// let sum = 0;
-
-// let i = 1;
-// while(i <= 10) {
-//     console.log(`sum = ${sum = sum + i}`);
-//     i++;
-// }
-// console.log(sum);
+let num = 5
+for (let i = 1; i <= 10; i++) {
+    console.log(num * i);
+}
 
 
 
-// // . Task 4: Write a program to print numbers from 10 to 1 using a while loop.
-
-// let j = 10;
-// while(j >= 1) {
-//     console.log(j);
-//     j--;
-// }
 
 
+// Activity 2: While Loop
 
-// // Activity 3: Do ... While Loop
+// . Task 3: Write a program to calculate the sum of numbers from 1 to 10 using a while loop.
 
-// // . Task 5: Write a program to print numbers from 1 to 5 using a do ... while loop.
+let sum = 0;
 
-// let k = 1;
-// do{
-//     console.log(k);
-//     k++;
-// }while(k <= 10)
+let i = 1;
+while(i <= 10) {
+    console.log(`sum = ${sum = sum + i}`);
+    i++;
+}
+console.log(sum);
 
 
 
-// // . Task 6: Write a program to calculate the factorial of a number using a do ... while loop.
+// . Task 4: Write a program to print numbers from 10 to 1 using a while loop.
+
+let j = 10;
+while(j >= 1) {
+    console.log(j);
+    j--;
+}
 
 
-// let n = 3;
-// let fact = 1;
 
-// for (let i = 1; i <= n; i++){
-//     fact = fact * i
-//     console.log(fact);
-// }
+// Activity 3: Do ... While Loop
 
-// console.log(`fact of ${n} = ${fact}`);
+// . Task 5: Write a program to print numbers from 1 to 5 using a do ... while loop.
+
+let k = 1;
+do{
+    console.log(k);
+    k++;
+}while(k <= 10)
 
 
-// // Activity 4: Nested Loops
 
-// // . Task 7: Write a program to print a pattern using nested for loops:
+// . Task 6: Write a program to calculate the factorial of a number using a do ... while loop.
 
-// let star = '';
 
-// for(let i = 1; i < 6; i++){
-//     for(let j = 0; j < i; j++){
-//         star =  "* " + star;
-//     }
-//     console.log(star);
-//     star = ''
-// }
+let n = 3;
+let fact = 1;
+
+for (let i = 1; i <= n; i++){
+    fact = fact * i
+    console.log(fact);
+}
+
+console.log(`fact of ${n} = ${fact}`);
+
+
+// Activity 4: Nested Loops
+
+// . Task 7: Write a program to print a pattern using nested for loops:
+
+let star = '';
+
+for(let i = 1; i < 6; i++){
+    for(let j = 0; j < i; j++){
+        star =  "* " + star;
+    }
+    console.log(star);
+    star = ''
+}
 
 
 // Activity 5: Loop Control Statements
